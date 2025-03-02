36ade86d34ad522b3da9ca52f695262f82b891dd
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-03-03 00:52:27 +0100

    fixup! fix(loader): add loaded module to `sys.modules`

--- src/copier_templates_extensions/extensions/loader.py
+++ src/copier_templates_extensions/extensions/loader.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
+import sys
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
-import sys
 from typing import TYPE_CHECKING, Any
 
 from copier.errors import UserMessageError
